4e5d2862fc76547635516c2b63695cce4eb15e69
Edgardo Luis Jimenez <edgardotecno@gmail.com>
2023-07-06 16:20:01 -0500

    Global Context

---
+++ src/Context/index.jsx
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+const ShoppingCartContext = createContext();
+
+const ShoppingCartProvider = ({ children }) => {
+  return;
+  <ShoppingCartContext.Provider>{children};</ShoppingCartContext.Provider>;
+};
+
+export { ShoppingCartProvider };
