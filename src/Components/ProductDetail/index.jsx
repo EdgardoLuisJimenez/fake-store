46c1285cae3f8396269aaaeee428def3ddc6895b
Edgardo Luis Jimenez <edgardotecno@gmail.com>
2023-07-06 21:11:38 -0500

    Make the initial aside of the layout

---
+++ src/Components/ProductDetail/index.jsx
@@ -0,0 +1,12 @@
+const ProductDetail = () => {
+  return (
+    <aside className="flex flex-col fixed right-0 border bg-white border-black rounded-lg w-[360px] h-[calc(100vh-80px)]">
+      <div className="flex justify-between items-center p-6">
+        <h2 className="font-medium text-xl">Detail</h2>
+        <div>x</div>
+      </div>
+    </aside>
+  );
+};
+
+export { ProductDetail };
