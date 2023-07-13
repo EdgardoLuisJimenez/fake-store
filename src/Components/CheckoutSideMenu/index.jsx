2d596d84cd1ff2cd959c167a07d24fc266a6ada9
Edgardo Luis Jimenez <edgardotecno@gmail.com>
2023-07-12 20:21:07 -0500

    MyOrders page: list of orders

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -30,6 +30,7 @@ const CheckoutSideMenu = () => {
 
     setOrder([...order, orderToAdd]);
     setCartProducts([]);
+    closeCheckSideMenuOpen();
   };
 
   return (
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,7 +1,28 @@
+import { useContext } from "react";
 import { Layout } from "../../Components/Layout";
+import { ShoppingCartContext } from "../../Context";
+import { Link } from "react-router-dom";
+import { OrdersCards } from "../../Components/OrdersCard";
 
 function MyOrders() {
-  return <Layout>MyOrders</Layout>;
+  const { order } = useContext(ShoppingCartContext);
+
+  return (
+    <Layout>
+      <div className="flex items-center justify-center relative w-80">
+        <h1>My Orders</h1>
+      </div>
+      {order.map((order, index) => {
+        <Link key={index} to={`/my-orders${order.id}`}>
+          <OrdersCards
+            totalPrice={order.totalPrice}
+            totalProducts={order.totalProducts}
+          />
+          ;
+        </Link>;
+      })}
+    </Layout>
+  );
 }
 
 export { MyOrders };
