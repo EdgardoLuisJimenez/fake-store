211b33aac2dac4510aa2d8edbbe0b806ffb1fef7
Edgardo Luis Jimenez <edgardotecno@gmail.com>
2023-07-27 12:43:10 -0500

    Add Shopping Cart Component

---
+++ src/Components/ShoppingCart/index.jsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { ShoppingCartContext } from "../../Context";
+import { ShoppingBagIcon } from "@heroicons/react/24/solid";
+
+const shoppingCart = () => {
+  const { closeProductDetail, cartProducts } = useContext(ShoppingCartContext);
+
+  const openCheckoutSideMenu = () => {
+    // openCheckSideMenuOpen()
+    closeProductDetail();
+  };
+
+  return (
+    <div
+      className="relative flex gap-0.5 items-center"
+      onClick={() => openCheckoutSideMenu}>
+      <ShoppingBagIcon className="w-6 h-6 fill-none stroke-black cursor-pointer" />
+      <div className="absolute -bottom-3.5 left-3.5 flex justify-center items-center rounded-full bg-black w-4 h-4 text-xs text-white">
+        {cartProducts.length}
+      </div>
+    </div>
+  );
+};
+
+export { shoppingCart };
